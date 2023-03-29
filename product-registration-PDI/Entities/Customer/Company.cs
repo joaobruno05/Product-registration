b98b54fb07c25b52216910619ebbab7f5e2fa284
Joao Vasconcelos <joao.vasconcelos@take.net>
2023-03-28 23:37:09 -0300

    Create Company and Person objects

--- product-registration-PDI/Entities/Customer/Company.cs
+++ product-registration-PDI/Entities/Customer/Company.cs
@@ -6,5 +6,21 @@ namespace product_registration_PDI.Entities
 {
     class Company : Customer
     {
+        public string Cnpj { get; set; }
+
+        public Company(string name, string adress, List<Product> products, string cnpj) : base(name, adress, products)
+        {
+            Cnpj = cnpj;
+        }
+
+        public bool validateCnpj(string cnpj)
+        {
+            string resultCnpj = cnpj.Replace(".", "");
+            resultCnpj = resultCnpj.Replace("-", "");
+
+            if (resultCnpj.Length != 14) return false;
+            return true;
+        }
     }
+}
 }
--- product-registration-PDI/Entities/Customer/Person.cs
+++ product-registration-PDI/Entities/Customer/Person.cs
@@ -1,11 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
 
 namespace product_registration_PDI.Entities
 {
     public class Person : Customer
     {
-        public string Register { get; set; }
+        public string Cpf { get; set; }
+        
+        public Person(string name, string adress, List<Product> products, string cpf) : base(name, adress, products)
+        {
+            Cpf = cpf;
+        }
+
+        public bool validateCpf(string cpf)
+        {
+            string resultCpf = cpf.Replace(".", "");
+            resultCpf = resultCpf.Replace("-", "");
+
+            if (resultCpf.Length != 11) return false;
+            return true;
+        }
     }
 }
