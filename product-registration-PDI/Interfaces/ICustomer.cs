5ce1f81c90a6b79d21acdc25af865e53fcd1bece
Joao Vasconcelos <joao.vasconcelos@take.net>
2023-04-06 23:05:58 -0300

    Add methods in the ICustomer interface

--- product-registration-PDI/Interfaces/ICustomer.cs
+++ product-registration-PDI/Interfaces/ICustomer.cs
@@ -6,7 +6,11 @@ namespace product_registration_PDI.Interfaces
     public interface ICustomer
     {
         public string Name { get; set; }
-        public string Adress { get; set; }
+        public string Address { get; set; }
         public List<Product> Products { get; set; }
+
+        public string CustomerPersonalData();
+        public void FormatProductsTable(List<Product> products);
+        public string TotalPrice(List<Product> products);
     }
 }
