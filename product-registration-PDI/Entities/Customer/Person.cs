0b61ac1c052b108ea509ebb15b6aafc5709fcc5e
Joao Vasconcelos <joao.vasconcelos@take.net>
2023-04-06 23:01:43 -0300

    Add contructors for the Person class and change the method name

--- product-registration-PDI/Entities/Customer/Person.cs
+++ product-registration-PDI/Entities/Customer/Person.cs
@@ -1,17 +1,28 @@
-﻿using System.Collections.Generic;
+﻿using product_registration_PDI.Interfaces;
+using System.Collections.Generic;
+using System.Globalization;
 
 namespace product_registration_PDI.Entities
 {
     public class Person : Customer
     {
         public string Cpf { get; set; }
-        
-        public Person(string name, string adress, List<Product> products, string cpf) : base(name, adress, products)
+
+        public Person(string cpf)
+        {
+            Cpf = cpf;
+        }
+        public Person(string name, string address, string cpf) : base(name, address)
         {
             Cpf = cpf;
         }
 
-        public bool validateCpf(string cpf)
+        public Person(string name, string address, List<Product> products, string cpf) : base(name, address, products)
+        {
+            Cpf = cpf;
+        }
+
+        public bool ValidateCpf(string cpf)
         {
             string resultCpf = cpf.Replace(".", "");
             resultCpf = resultCpf.Replace("-", "");
