3069a69395784620471f5c3e2b8ab0fac28f03d8
Joao Vasconcelos <joao.vasconcelos@take.net>
2023-04-06 22:57:09 -0300

    Add contructors in the Company class

--- product-registration-PDI/Entities/Customer/Company.cs
+++ product-registration-PDI/Entities/Customer/Company.cs
@@ -4,11 +4,25 @@ using System.Text;
 
 namespace product_registration_PDI.Entities
 {
-    class Company : Customer
+    public class Company : Customer
     {
         public string Cnpj { get; set; }
 
-        public Company(string name, string adress, List<Product> products, string cnpj) : base(name, adress, products)
+        public Company()
+        {
+        }
+
+        public Company(string cnpj)
+        {
+            Cnpj = cnpj;
+        }
+
+        public Company(string name, string address, string cnpj) : base(name, address)
+        {
+            Cnpj = cnpj;
+        }
+
+        public Company(string name, string address, List<Product> products, string cnpj) : base(name, address, products)
         {
             Cnpj = cnpj;
         }
@@ -16,11 +30,11 @@ namespace product_registration_PDI.Entities
         public bool validateCnpj(string cnpj)
         {
             string resultCnpj = cnpj.Replace(".", "");
+            resultCnpj = resultCnpj.Replace("/", "");
             resultCnpj = resultCnpj.Replace("-", "");
 
             if (resultCnpj.Length != 14) return false;
             return true;
         }
     }
-}
 }
